e271cf6bd15467cdd7c5dcfda6d4aff5d1adb91d
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-05-17 00:45:59 -0300

    change the webHistory to webHasHistory

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import MovieTicketBuy from '@/components/modules/movieTickets/views/MovieTicketB
 import MainPage from '@/components/modules/mainPages/mainPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
