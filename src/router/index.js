9cf9dac6324e2f286a5ab3d7edce5ba34a9a2b14
DiegoVargasFalla <fallaandres1007f@gmail.com>
2025-07-02 22:08:35 -0300

    change webHashHistory to webHistory

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import MainPage from '@/components/modules/mainPages/mainPage.vue'
 import Login from '@/components/modules/mainPages/Login.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
